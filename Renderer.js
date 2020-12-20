c5d3383a868329bc4bdeab2a0f212fba08275f84
HealthyJunkie <androwas101@gmail.com>
2020-12-20 07:33:35 +0200

    polish + gitignore

--- Renderer.js
+++ Renderer.js
@@ -7,19 +7,19 @@ class Renderer{
 
         Handlebars.registerHelper('populate', val => {
             if(val === 'B')
-                return new Handlebars.SafeString(this.block_el('btn-small orange darken-4 transparent-text'));
+                return this.block_el('btn-small orange darken-4 transparent-text')
             if(val === '1')
-                return new Handlebars.SafeString(this.el('btn-floating  waves-effect waves-light blue darken-3'));
+                return this.el('btn-floating  waves-effect waves-light blue darken-3')
             if(val === '2')
-                return new Handlebars.SafeString(this.el('btn-floating waves-effect waves-light red darken-3'));
+                return this.el('btn-floating waves-effect waves-light red darken-3')
             if(val === 'C')
-                return new Handlebars.SafeString(this.el('btn-floating btn-small yellow '));
-            return new Handlebars.SafeString(this.el('btn-floating transparent btn-medium z-depth-0'));
+                return this.el('btn-floating btn-small yellow ')
+            return this.el('btn-floating transparent btn-medium z-depth-0')
         })
     }
     
-    el = classs => `<div class="${classs}"></div>`
-    block_el = classs => `<div class="${classs}">-------------</div>`
+    el = classs => new Handlebars.SafeString(`<div class="${classs}"></div>`)
+    block_el = classs => new Handlebars.SafeString(`<div class="${classs}">-------------</div>`)
 
     _handleBarAppender = (elementToAppendTo, Template, data) => {
         let newHTML = Template(data);
